e7b6dcab7484af0c32ca14a70828022854dc84df
liliiamarkova <liliyamarkova9301@gmail.com>
2025-01-15 17:39:42 +0200

    add adbanner

--- sonic_helpers/src/commonMain/kotlin/com/ato/helpers/AdManager.kt
+++ sonic_helpers/src/commonMain/kotlin/com/ato/helpers/AdManager.kt
@@ -1,6 +1,10 @@
 package com.ato.helpers
 
+import androidx.compose.runtime.Composable
+
 interface AdManager {
     fun loadAd(adUnitId: String)
     fun showAd()
+    @Composable
+    fun AdMobBanner()
 }
